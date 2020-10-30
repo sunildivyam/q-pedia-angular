e0b5e77512a89a9ece7d182e23db9f9c4c086b81
Sunil Kumar <sunil.divyam@gmail.com>
2020-10-30 12:10:48 +0530

    Package json updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { QPediaNgLibModule } from '../q-pedia-ng-lib/q-pedia-ng-lib.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, QPediaNgLibModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/q-pedia-ng-lib/q-pedia-ng-lib.module.ts
+++ src/q-pedia-ng-lib/q-pedia-ng-lib.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: []
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
 })
-export class QPediaNgLibModule {}
+export class QPediaNgLibModule { }
